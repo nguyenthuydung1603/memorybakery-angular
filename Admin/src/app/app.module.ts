65ce90733f06aaaf69f1c0527af6ae170b95702b
tranthianhtruc <tructta20406@st.uel.edu.vn>
2023-04-21 22:32:44 +0700

    Quản lý đơn hàng, Chi tiết đơn hàng

--- Admin/src/app/app.module.ts
+++ Admin/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
+import { ChiTietDonHangComponent } from './chi-tiet-don-hang/chi-tiet-don-hang.component';
+import { QuanLyDonHangComponent } from './quan-ly-don-hang/quan-ly-don-hang.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    HeaderComponent
+    HeaderComponent,
+    ChiTietDonHangComponent,
+    QuanLyDonHangComponent
   ],
   imports: [
     BrowserModule,

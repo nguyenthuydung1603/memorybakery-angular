33ceca7eafc06b7eaace570178a0c1e4a05fd0a7
memoryBakery <117665370+memoryBakery@users.noreply.github.com>
2023-05-03 22:58:02 +0700

    update Admin Account

--- Admin/src/app/app-routing.module.ts
+++ Admin/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProductManagementComponent } from './product-management/product-management.component';
 import { PromotionManagementComponent } from './promotion-management/promotion-management.component';
 import { BlogManagementComponent } from './blog-management/blog-management.component';
+import { CustomerManagementComponent } from './customer-management/customer-management.component';
 
 const routes: Routes = [
   {path:"dashboard",component:DashboardComponent},
   {path: 'product-management', component:ProductManagementComponent },
   {path: 'promotion', component:PromotionManagementComponent },
-  {path: 'blog', component:BlogManagementComponent}
+  {path: 'blog', component:BlogManagementComponent},
+  {path: 'customer', component:CustomerManagementComponent}
 ];
 
 @NgModule({

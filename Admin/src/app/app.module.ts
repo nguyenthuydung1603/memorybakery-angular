0eec7c465e84d9b77de8be00dec2ba4f7471efae
memoryBakery <117665370+memoryBakery@users.noreply.github.com>
2023-05-12 10:42:04 +0700

    cài https://ckeditor.com/ckeditor-5/angular/

--- Admin/src/app/app.module.ts
+++ Admin/src/app/app.module.ts
@@ -19,6 +19,7 @@ import { PromotionDetailComponent } from './promotion-detail/promotion-detail.co
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { QuanLyDonHangOrderStatusComponent } from './quan-ly-don-hang-order-status/quan-ly-don-hang-order-status.component';
 import { SettingComponent } from './setting/setting.component';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 
 
 @NgModule({
@@ -45,6 +46,7 @@ import { SettingComponent } from './setting/setting.component';
     HttpClientModule,
     AngularEditorModule,
     SweetAlert2Module.forRoot(),
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
